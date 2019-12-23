4501bd4c6352e904c0575aeb93b5dcb9b8befca9
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2019-12-23 17:16:41 +0000

    Update image_vision.py

--- image_vision.py
+++ image_vision.py
@@ -12,11 +12,12 @@ cv2.createTrackbar("U-H", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("U-V", "Trackbars", 255, 255, nothing)
 
-
+#font = cv2.initFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 3, 8)
+font = cv2.FONT_HERSHEY_COMPLEX
 #hsv = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
 
 while True:
-    img = cv2.imread('lena.png')
+    img = cv2.imread('idn1.png')
     hsv = cv2.cvtColor( img, cv2.COLOR_BGR2HSV)
 
     l_h = cv2.getTrackbarPos("L-H", "Trackbars")
@@ -34,16 +35,22 @@ while True:
 
     _,contours, hierarcy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-
+    jumlah = int (0)
     for cnt in contours:    
-        cv2.drawContours(res, [cnt], 0, (0,0,0), 5)
+        #cv2.drawContours(res, [cnt], 0, (0,0,0), 5)
         
         x,y,w,h = cv2.boundingRect(cnt)
         ret,thresh1 = cv2.threshold(res,127,255,cv2.THRESH_BINARY_INV)
         
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
 
+        if w*h > 1000:
+            #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+            jumlah = jumlah+1
+
+    #print(jumlah+1)
+    cv2.putText(img,"Jumlah : " + str(jumlah), (50,15),font,0.55,(0,0,255),1)
     cv2.imshow('image',img)
+    
     # cv2.imshow('res',res)
 
     key = cv2.waitKey(1)
