0dbe1438ade629e93bd9028d2bfac736838e0559
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-02-10 08:05:15 +0700

    video capture

---
+++ video_capture.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _,frame = cap.read(0)
+    cv2.imshow("frame", frame)
+    
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
